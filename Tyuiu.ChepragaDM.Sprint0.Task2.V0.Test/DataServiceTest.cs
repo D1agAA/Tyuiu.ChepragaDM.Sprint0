fc1ad09aceb92ea128c21542bf186edc3b467818
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-10 22:11:41 +0500

    Task2

---
+++ Tyuiu.ChepragaDM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChepragaDM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ChepragaDM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Dima";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Dima", res);
+        }
+    }
+}
