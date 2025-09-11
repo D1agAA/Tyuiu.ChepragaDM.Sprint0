14b289bbcf4fb108ac91b4fae1553dca703eb673
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-11 19:47:33 +0500

    Task3

---
+++ Tyuiu.ChepragaDM.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ChepragaDM.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.ChepragaDM.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TCheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
