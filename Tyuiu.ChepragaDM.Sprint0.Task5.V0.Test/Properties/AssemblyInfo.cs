82f14e95a419637da6e1ef2677d0174e2eabe6dd
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-11 20:39:01 +0500

    Task5

---
+++ Tyuiu.ChepragaDM.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChepragaDM.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChepragaDM.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5cc2df9-082b-48fa-ae3f-fbedb0d06c84")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ChepragaDM.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ChepragaDM.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ChepragaDM.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a + b = " + DataService.Addition(10, 10));
+            Console.WriteLine("a - b = " + DataService.Substraction(8, 2));
+            Console.WriteLine("a * b = " + DataService.Multiplication(6, 7));
+            Console.WriteLine("a / b = " + DataService.Division(100, 0));
+            Console.ReadKey();
+        }
+    }
+}
